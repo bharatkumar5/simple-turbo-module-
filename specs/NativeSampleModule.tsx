6f3fac146894df5f9c84c5608aa034c1888ed927
bharat kumar <bharatkumar18002@gmail.com>
2024-11-30 20:26:37 +0530

    first commit

---
+++ specs/NativeSampleModule.tsx
@@ -0,0 +1,11 @@
+
+
+import {TurboModule, TurboModuleRegistry} from 'react-native';
+
+export interface Spec extends TurboModule {
+  readonly reverseString: (input: string) => string;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>(
+  'NativeSampleModule',
+);
